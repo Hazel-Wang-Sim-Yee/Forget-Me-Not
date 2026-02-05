b5ddb9808f291debed8c01ea56e183e2bb6bec54
Hazel Wang Sim Yee <S10268196@connect.np.edu.sg>
2026-02-05 13:24:28 +0800

    NPC Nav fixed (somewhat)
    NPC does not turn but it is no longer going backwards

--- Assets/Scripts/NPCBehaviour.cs
+++ Assets/Scripts/NPCBehaviour.cs
@@ -4,7 +4,9 @@ using UnityEngine.AI;
 public class NPCBehaviour : MonoBehaviour
 {
     private NavMeshAgent agent;
-
+    float rotationSpeed = 5f;
+    Vector3 moveDirection;
+    
     private void Awake()
     {
         agent = GetComponent<NavMeshAgent>();
@@ -14,7 +16,25 @@ public class NPCBehaviour : MonoBehaviour
     {
         if (destination != null)
         {
+            Debug.Log("Setting destination to: " + destination.position);
             agent.SetDestination(destination.position);
         }
+    }
+
+    void Update()
+    {
+        if (agent.velocity.sqrMagnitude > 0.1f)
+        {
+            Vector3 moveDirection = agent.velocity.normalized;
+            RotateTowardsMovementDirection();
+        }
+        
+    }
+
+    void RotateTowardsMovementDirection()
+    {
+        moveDirection.y = 0; // Keep only horizontal rotation
+        Quaternion targetRotation = Quaternion.LookRotation(moveDirection);
+        transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
     }
 }
